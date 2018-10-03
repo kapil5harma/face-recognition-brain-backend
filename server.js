e9031249ddf74b35c567077f3566e341b982f4a8
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-03 16:57:10 +0530

    Updating entries in "/image" route

--- server.js
+++ server.js
@@ -121,19 +121,20 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   // console.log('req.body: ', req.body);
-  console.log('Entered "/image"');
+  // console.log('Entered "/image"');
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json('No Such User');
-  }
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      if (entries.length) {
+        res.status(200).json(entries[0]);
+      } else {
+        res.status(404).json('User Not Found');
+      }
+    })
+    .catch(err => res.status(400).json('Cannot get entries count.'));
 });
 
 // Bcrypt Hashing of passwords
