9a80bf725d7b14c46bd5896485dd9e3a460e4e91
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-07 22:01:10 +0530

    Refactored server.js -- controllers moved to their separate files

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const knex = require('knex');
 
 const db = knex({
@@ -34,97 +39,18 @@ app.get('/', (req, res) => {
   res.json(database.users);
 });
 
-app.post('/signin', (req, res) => {
-  // console.log('Post /signin');
-  // console.log('req: ', req.body);
-  // res.json(`${req.url} is working!`);
-
-  const { email, password } = req.body;
-  db.select('email', 'hash')
-    .from('login')
-    .where('email', '=', email)
-    .then(data => {
-      // console.log('\n******\n[/signin]\ndata: ', data);
-      const isValid = bcrypt.compareSync(password, data[0].hash);
-      // console.log('isValid: ', isValid);
-      if (isValid) {
-        return db
-          .select('*')
-          .from('users')
-          .where('email', '=', email)
-          .then(user => {
-            // console.log('user: ', user);
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json('User Not Found'));
-      } else {
-        res.status(400).json('Incorrect email and/or password');
-      }
-    })
-    .catch(err => res.status(400).json('Incorrect email and/or password'));
-});
+app.post('/signin', signin.handleSignin(db, bcrypt)(req, res));
 
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-
-  const hash = bcrypt.hashSync(password);
-
-  // bcrypt.compareSync('bacon', hash); // true
-  // bcrypt.compareSync('veggies', hash); // false
-  db.transaction(trx => {
-    trx
-      .insert({ hash: hash, email: email })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json('Email already exists'));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*')
-    .from('users')
-    .where({ id: id })
-    .then(user => {
-      // console.log('\n ***** ***** ***** ***** ***** \n');
-      // console.log('[/profile/:id] user:\n\n', user[0]);
-      // console.log('\n ***** ***** ***** ***** ***** \n');
-      if (user.length) {
-        res.status(200).json(user[0]);
-      } else {
-        res.status(404).json('User Not Found');
-      }
-    })
-    .catch(err => res.status(404).json('Error getting User'));
+app.get('/profile/:id', (req, res, db) => {
+  profile.handleProfileGet(req, res, db);
 });
 
 app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      if (entries.length) {
-        res.status(200).json(entries[0]);
-      } else {
-        res.status(404).json('User Not Found');
-      }
-    })
-    .catch(err => res.status(400).json('Cannot get entries count.'));
+  image.handleImage(req, res, db);
 });
 
 app.listen(port, () => {
