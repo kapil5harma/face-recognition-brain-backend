98f6fe10347dd41ee5f4ccc094f9e47d2f312b8c
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-07 21:58:38 +0530

    Refactored signin route/controller out of server.js

---
+++ controllers/signin.js
@@ -0,0 +1,33 @@
+const handleSignin = (db, bcrypt) => (req, res) => {
+  // console.log('Post /signin');
+  // console.log('req: ', req.body);
+  // res.json(`${req.url} is working!`);
+
+  const { email, password } = req.body;
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then(data => {
+      // console.log('\n******\n[/signin]\ndata: ', data);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      // console.log('isValid: ', isValid);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then(user => {
+            // console.log('user: ', user);
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json('User Not Found'));
+      } else {
+        res.status(400).json('Incorrect email and/or password');
+      }
+    })
+    .catch(err => res.status(400).json('Incorrect email and/or password'));
+};
+
+module.exports = {
+  handleSignin: handleSignin
+};
