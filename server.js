a4c0bc16a76f7250420c342927a634ee9d38c170
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-23 00:00:46 +0530

    DB setup to be used by Heroku

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'kapil5harma',
-    password: '',
-    database: 'face-recognition-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
